19e4d03d6682ad0d36191cb6ebd58fde3cefa4de
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2014-11-03 20:58:18 -0800

    Copy script-cat.pl to /usr/bin, modify wrapsuid.c appropriately

--- assign6/wrapsuid.c
+++ assign6/wrapsuid.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 
-#define SCRIPT_PATH "/home/bolokhov/comp270/assign6/script-cat.pl"
+#define SCRIPT_PATH "/usr/bin/script-cat.pl"
 
 int main(int argc, char *argv[]) {
   return execv(SCRIPT_PATH, argv);
